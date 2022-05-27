fb1bf67c76e0200cae3f6ac249622298f545ddec
Abhinav0055 <abhinav0055.be21@chitkara.edu.in>
2022-05-27 12:02:26 +0530

    commit:2 in master

--- Complex Sum.cpp
+++ Complex Sum.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class complex{
-    int real,imag;
-    public:
-    complex(){
-        real=imag=0;
-    }
-    complex(int r, int i){
-        real=r,imag=i;
-    }
-   friend complex sum(complex,complex);
-   void disp(){
-       cout<<real<<"+"<<imag<<"i"<<endl;
-   }
+// class complex{
+//     int real,imag;
+//     public:
+//     complex(){
+//         real=imag=0;
+//     }
+//     complex(int r, int i){
+//         real=r,imag=i;
+//     }
+//    friend complex sum(complex,complex);
+//    void disp(){
+//        cout<<real<<"+"<<imag<<"i"<<endl;
+//    }
     
-};
+// };
 
     complex sum(complex C1,complex C2){
      complex C3;
