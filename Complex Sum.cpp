6f53d2e75e9eb2d6d430ca24fb6d53ba04050543
Abhinav0055 <abhinav0055.be21@chitkara.edu.in>
2022-05-27 11:42:10 +0530

    commit

--- Complex Sum.cpp
+++ Complex Sum.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 using namespace std;
 
@@ -18,10 +10,7 @@ class complex{
     complex(int r, int i){
         real=r,imag=i;
     }
-    friend complex sum(complex,complex);
-    void disp(){
-        cout<<real<<"+"<<imag<<"i"<<endl;
-    }
+    
 };
 
  complex sum(complex C1,complex C2){
